ca2e30ec687c7794df7dd629203f4be35001b9cd
eleuterio-coding <rafael.eleuterio@gmail.com>
2026-08-24 15:23:40 -0300

    Limit Wealth by Level to character creation sessions

---
+++ scripts/character-builder/wealth-purchase-creation-ui.js
@@ -0,0 +1,2 @@
+import{initWealthPurchaseUi}from'./wealth-purchase-ui.js?v=20260824-wealth-by-level1';
+export function initWealthPurchaseCreationUi(){if(new URLSearchParams(location.search).has('id'))return;return initWealthPurchaseUi()}
