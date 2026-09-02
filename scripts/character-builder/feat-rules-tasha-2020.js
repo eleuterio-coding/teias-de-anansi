331a6feccf77fdff1f71269152c544c7c8495da4
eleuterio-coding <rafael.eleuterio@gmail.com>
2026-09-02 00:18:02 -0300

    Bloco 9: restaurar regras mecânicas de talentos Tasha

---
+++ scripts/character-builder/feat-rules-tasha-2020.js
@@ -0,0 +1,7 @@
+export const TASHA_2020_FEAT_RULES={
+ 'Artificer Initiate':{},
+ 'Eldritch Adept':{},
+ 'Fighting Initiate':{},
+ 'Gunner':{ability:{options:['Destreza'],amount:1},weaponTraining:['Armas de fogo'],combatFlags:['gunnerIgnoreLoading','gunnerNoCloseRangeDisadvantage']},
+ 'Metamagic Adept':{}
+};
