f02ba599cf82ee6beadb7369e03c80b21e926962
eleuterio-coding <rafael.eleuterio@gmail.com>
2026-09-02 11:41:40 -0300

    Bloco 12: carregar alvo ativo do encontro na Ficha

--- scripts/character-sheet-equipment-ownership.js
+++ scripts/character-sheet-equipment-ownership.js
@@ -63,4 +63,5 @@ if(typeof document!=='undefined'){
  import('./character-sheet-resolution-ui.js?v=20260902-resolution1');
  import('./character-sheet-magic-item-resolution-ui.js?v=20260902-resolution1');
  import('./character-sheet-death-ui.js?v=20260902-resolution1');
+ import('./character-sheet-encounter-target.js?v=20260902-encounters1');
 }
