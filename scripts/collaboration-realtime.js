f95142f6c8c5735d212e358258c81db54418b1b1
eleuterio-coding <rafael.eleuterio@gmail.com>
2026-09-14 11:47:09 -0300

    chore: usar modelo de acesso por login no tempo real

--- scripts/collaboration-realtime.js
+++ scripts/collaboration-realtime.js
@@ -1,6 +1,6 @@
-import{createFirebaseCollaborationProvider}from'./firebase-collaboration-provider.js?v=20260914-party-visibility1';
-import{pullCollaborations}from'./collaboration-sync.js?v=20260914-party-visibility1';
-import{readCollaborationSession,writeCollaborationSession,readCollaborationCache}from'./collaboration-view.js?v=20260914-party-visibility1';
+import{createFirebaseCollaborationProvider}from'./firebase-collaboration-provider.js?v=20260914-login-context1';
+import{pullCollaborations}from'./collaboration-sync.js?v=20260914-login-context1';
+import{readCollaborationSession,writeCollaborationSession,readCollaborationCache}from'./collaboration-view.js?v=20260914-login-context1';
 import{CAMPAIGN_KEY,readCampaigns}from'./campaign-state.js?v=20260910-realtime1';
 import{ADVENTURE_KEY,readAdventures}from'./adventure-state.js?v=20260910-realtime1';
 import{KEY as CHARACTER_KEY,read as readCharacters}from'./character-builder/state.js';
