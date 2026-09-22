9a787b641c2b95b483dc2d82ea39f0f24c0ae131
eleuterio-coding <rafael.eleuterio@gmail.com>
2026-09-22 15:25:30 -0300

    Recarrega curva ampliada de PO

--- scripts/character-builder/wealth-purchase-creation-ui.js
+++ scripts/character-builder/wealth-purchase-creation-ui.js
@@ -1,2 +1,2 @@
-import{initWealthPurchaseUi}from'./wealth-purchase-ui.js?v=20260922-wealth-class1';
+import{initWealthPurchaseUi}from'./wealth-purchase-ui.js?v=20260922-wealth-class2';
 export function initWealthPurchaseCreationUi(){if(new URLSearchParams(location.search).has('id'))return;return initWealthPurchaseUi()}
