5095dc14413f34f3dfba0e5698a2660e9302d064
eleuterio-coding <rafael.eleuterio@gmail.com>
2026-09-22 15:37:38 -0300

    Atualiza versão da progressão ampliada de PO

--- scripts/character-builder/wealth-purchase-creation-ui.js
+++ scripts/character-builder/wealth-purchase-creation-ui.js
@@ -1,2 +1,2 @@
-import{initWealthPurchaseUi}from'./wealth-purchase-ui.js?v=20260922-wealth-class2';
+import{initWealthPurchaseUi}from'./wealth-purchase-ui.js?v=20260922-wealth-class3';
 export function initWealthPurchaseCreationUi(){if(new URLSearchParams(location.search).has('id'))return;return initWealthPurchaseUi()}
