cfd30683f242bddfa91163327a31d8c0bcab958b
eleuterio-coding <rafael.eleuterio@gmail.com>
2026-09-23 11:27:59 -0300

    Atualizar UI de idiomas para regras raciais

--- scripts/character-builder/language-ui.js
+++ scripts/character-builder/language-ui.js
@@ -1,5 +1,5 @@
 import{state,$,esc,fold}from'./state.js';
-import{languageOutcome,sanitizeLanguageChoices}from'./language-mechanics.js';
+import{languageOutcome,sanitizeLanguageChoices}from'./language-mechanics.js?v=20260923-harpy-aquan1';
 
 let queued=false;
 function ensureHost(){
