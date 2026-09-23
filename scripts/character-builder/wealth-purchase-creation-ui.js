ebb7d8ea085bc27bc24414fb73e78ffff9edbb72
eleuterio-coding <rafael.eleuterio@gmail.com>
2026-09-23 11:44:18 -0300

    Atualiza catálogo de compras fechado por padrão

--- scripts/character-builder/wealth-purchase-creation-ui.js
+++ scripts/character-builder/wealth-purchase-creation-ui.js
@@ -1,2 +1,2 @@
-import{initWealthPurchaseUi}from'./wealth-purchase-ui.js?v=20260922-wealth-class4';
+import{initWealthPurchaseUi}from'./wealth-purchase-ui.js?v=20260923-wealth-collapse1';
 export function initWealthPurchaseCreationUi(){if(new URLSearchParams(location.search).has('id'))return;return initWealthPurchaseUi()}
