646f5967635a7ef5f05f5d94a1fa320e48e5318b
eleuterio-coding <rafael.eleuterio@gmail.com>
2026-09-23 10:39:21 -0300

    Atualiza cache da riqueza por nível ampliada

--- scripts/character-builder/wealth-purchase-creation-ui.js
+++ scripts/character-builder/wealth-purchase-creation-ui.js
@@ -1,2 +1,2 @@
-import{initWealthPurchaseUi}from'./wealth-purchase-ui.js?v=20260922-wealth-class3';
+import{initWealthPurchaseUi}from'./wealth-purchase-ui.js?v=20260922-wealth-class4';
 export function initWealthPurchaseCreationUi(){if(new URLSearchParams(location.search).has('id'))return;return initWealthPurchaseUi()}
