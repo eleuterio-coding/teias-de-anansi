eb3dd99f8a82bbf9a23b27e18e00dd555a9df494
eleuterio-coding <rafael.eleuterio@gmail.com>
2026-09-23 11:28:11 -0300

    Atualizar versão do módulo de Raças

--- scripts/module-clean-loader.js
+++ scripts/module-clean-loader.js
@@ -1,6 +1,6 @@
 (()=>{
   'use strict';
-  const VERSION='20260923-native-languages1';
+  const VERSION='20260923-harpy-aquan1';
   const LAYOUT='biblioteca-light.css?v=20260827-library-layout2';
   const key=document.documentElement.dataset.cleanModule;
   const raceRules=[
