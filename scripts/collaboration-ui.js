fb30f09750bbb1affa56f57583d7b9ea01700fb7
eleuterio-coding <rafael.eleuterio@gmail.com>
2026-09-25 12:37:54 -0300

    chore: atualizar colaboração para mídia

--- scripts/collaboration-ui.js
+++ scripts/collaboration-ui.js
@@ -1,8 +1,8 @@
-import{createFirebaseCollaborationProvider,loadFirebaseConfig}from'./firebase-collaboration-provider.js?v=20260917-global-realtime1';
-import{readCampaigns}from'./campaign-state.js?v=20260924-session-encounters1';
-import{readAdventures}from'./adventure-state.js?v=20260924-session-scenes2';
+import{createFirebaseCollaborationProvider,loadFirebaseConfig}from'./firebase-collaboration-provider.js?v=20260925-media1';
+import{readCampaigns}from'./campaign-state.js?v=20260925-media1';
+import{readAdventures}from'./adventure-state.js?v=20260925-media1';
 import{read as readCharacters}from'./character-builder/state.js';
-import{syncAll,publishLocalCampaign}from'./collaboration-sync.js?v=20260917-global-realtime1';
+import{syncAll,publishLocalCampaign}from'./collaboration-sync.js?v=20260925-media1';
 import{writeCollaborationSession,clearCollaborationSession}from'./collaboration-view.js?v=20260917-player-catalog1';
 import{revokeRemoteMembership}from'./firebase-realtime-ops.js?v=20260917-global-realtime1';
 
